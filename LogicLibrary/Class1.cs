4ca0bc745fda1417ff258ecc904f528f9e9d4a8f
Nigger <mister.jorek@mail.ru>
2021-03-10 15:54:20 +0300

    Reflection commit

---
+++ LogicLibrary/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace testAppForDigDes
+{  
+    public class Class1
+    { 
+        private List<string> method(string n)
+        {
+            FileStream file = new FileStream(n, FileMode.Open);    
+            StreamReader readFile = new StreamReader(file);        
+            List<string> lines = new List<string>();
+            while (!readFile.EndOfStream)
+            {
+                string s = readFile.ReadLine().ToLower();
+                string[] words = s.Split(new char[] { ' ', ',', '.', '\0', '-', '—', '\"', '&', '*',
+                                                      '!', '?', '(', ')', '\n', '»', '«', ':', ';' });
+                foreach (string word in words)
+                    if (word != "" && word != null)
+                        lines.Add(word);
+            }
+            readFile.Close();
+
+            var k = lines.GroupBy(g => g).OrderByDescending(g => g.Count()).Select(g => $"{g.Key} {g.Count()}").ToList();
+            return k;
+        }
+    }
+}
---
+++ testAppForDigDes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace testAppForDigDes 
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 c = new Class1();
+            var type = typeof(Class1);
+            var atr = type.GetMethod("method", BindingFlags.NonPublic | BindingFlags.Instance);
+            List<string> finishList = (List<string>)atr.Invoke(c, new string[]{ "C:/Users/ASUS/Desktop/Текстовый файл сомнительного содержания.txt" });
+            using (StreamWriter w = new StreamWriter("C:/Users/ASUS/Desktop/итог.txt")) {
+                foreach(var str in finishList)
+                   w.WriteLine(str);
+            }
+        }
+    }
+}
