011b2904e06a51e337ff5d3435deae4effa214c7
Nigger <mister.jorek@mail.ru>
2021-03-14 20:43:51 +0300

    я делал через API-Controller в WebAPI

---
+++ ClientDictionary/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ClientDictionary
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            string newStr = "";
+            FileStream file = new FileStream("C:/Users/ASUS/Desktop/текстовый файл сомнительного содержания.txt", FileMode.Open);
+            using (StreamReader readFile = new StreamReader(file))
+            {
+                s = readFile.ReadToEnd().ToLower();
+            }
+            using (var webClient = new WebClient())
+            {
+                webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+                s = AddaptJson(s);
+                newStr = webClient.UploadString("http://localhost:56155/api/Dictionary", "Post", "\""+ s+ "\"");
+            }
+            var lastDiction = JsonConvert.DeserializeObject<Dictionary<string, int>>(newStr);
+
+            using (StreamWriter w = new StreamWriter("C:/Users/ASUS/Desktop/итог.txt"))
+            {
+                foreach (var str in lastDiction)
+                    w.WriteLine($"{str.Value} {str.Key}");
+            }
+        }
+        public static string AddaptJson(string value)
+        {
+            value = value.Replace('\n', ' ');
+            value = value.Replace('\r', ' ');
+            value = value.Replace('\"', ' ');
+            value = value.Replace('/', ' ');
+            value = value.Replace('\\', ' ');
+            return value;
+        }
+    }
+}
---
+++ WebApiDictionary/Controllers/DictionaryController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiDictionary.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DictionaryController : ControllerBase
+    {
+        // POST api/<DictionaryController>
+        [HttpPost]
+        public Dictionary<string, int> Post([FromBody] string s)
+        {
+            List<string> lines = new List<string>();
+
+            string[] words = s.Split(new char[] { ' ', ',', '.', '\0', '-', '—', '\"', '&', '*',
+                                                  '!', '?', '(', ')', '\n', '»', '«', ':', ';' });
+            foreach (string word in words)
+                if (word != "" && word != null && word != "\r")
+                    lines.Add(word);
+
+            var dict = new Dictionary<string, int>();
+            foreach (string word in lines)
+            {
+                if (dict.ContainsKey(word))
+                {
+                    dict[word]++;
+                }
+                else
+                {
+                    dict[word] = 1;
+                }
+            }
+
+            return dict.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+        }
+    }
+}
