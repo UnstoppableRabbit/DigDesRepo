13a8b1507b8518d164e3afa375961a839fa7fbf0
Nigger <mister.jorek@mail.ru>
2021-03-14 15:40:33 +0300

    rasparallelil blin...(

--- LogicLibrary/Class1.cs
+++ LogicLibrary/Class1.cs
@@ -3,20 +3,57 @@ using System.Collections.Generic;
 using System.Text;
 using System.IO;
 using System.Linq;
+using System.Diagnostics;
+using System.Threading;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
 
 namespace testAppForDigDes
 {  
     public class Class1
-    { 
-        private Dictionary<string, int> method(string s)
+    {
+        private static object _key = new object();
+        ConcurrentDictionary<string, int> dict = new ConcurrentDictionary<string, int>();
+        List<string> lines = new List<string>();
+        public Dictionary<string, int> PublicMethod(string s)
         {
-             
-            List<string> lines = new List<string>();            
-                
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
             string[] words = s.Split(new char[] { ' ', ',', '.', '\0', '-', '—', '\"', '&', '*',
                                                   '!', '?', '(', ')', '\n', '»', '«', ':', ';' });
             foreach (string word in words)
-                if (word != "" && word != null && word !="\r") 
+                if (word != "" && word != null && word != "\r")
+                    lines.Add(word);
+
+            Parallel.ForEach(lines, word =>
+            {
+                if (dict.ContainsKey(word))
+                {
+                    lock(_key)
+                        dict[word]++;
+                }
+                else
+                {
+                    lock(_key)
+                        dict[word] = 1;
+                }
+            });
+            var k = dict.AsParallel().OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            sw.Stop();
+            Console.WriteLine("Параллельно: " + sw.ElapsedMilliseconds);
+            return k;
+        }
+        private Dictionary<string, int> PrivateMethod(string s)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            string[] words = s.Split(new char[] { ' ', ',', '.', '\0', '-', '—', '\"', '&', '*',
+                                                  '!', '?', '(', ')', '\n', '»', '«', ':', ';' });
+            foreach (string word in words)
+                if (word != "" && word != null && word != "\r")
                     lines.Add(word);
 
             var dict = new Dictionary<string, int>();
@@ -31,8 +68,11 @@ namespace testAppForDigDes
                     dict[word] = 1;
                 }
             }
-            
-            return dict.OrderByDescending(x => x.Value).ToDictionary(x=>x.Key, x=>x.Value);
+
+            var k = dict.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            sw.Stop();
+            Console.WriteLine("Не параллельно: " + sw.ElapsedMilliseconds);
+            return k;
         }
     }
 }
--- testAppForDigDes/Program.cs
+++ testAppForDigDes/Program.cs
@@ -11,18 +11,19 @@ namespace testAppForDigDes
             string s = "";
             Class1 c = new Class1();
             var type = typeof(Class1);
-            var atr = type.GetMethod("method", BindingFlags.NonPublic | BindingFlags.Instance);
+            var atr = type.GetMethod("PrivateMethod", BindingFlags.NonPublic | BindingFlags.Instance);
           
             FileStream file = new FileStream("C:/Users/ASUS/Desktop/текстовый файл сомнительного содержания.txt", FileMode.Open);
             using (StreamReader readFile = new StreamReader(file))
             {
                  s = readFile.ReadToEnd().ToLower();
             }
-            Dictionary<string, int> finishList = (Dictionary<string,int>)atr.Invoke(c, new string[] { s });
-
-            using (StreamWriter w = new StreamWriter("C:/Users/ASUS/Desktop/итог.txt")) {
+            // Dictionary<string, int> finishList = (Dictionary<string,int>)atr.Invoke(c, new string[] { s });
+             Dictionary<string, int> finishList = c.PublicMethod(s);
+ 
+            using (StreamWriter w = new StreamWriter("C:/Users/ASUS/Desktop/итог1.txt")) {
                 foreach(var str in finishList)
-                   w.WriteLine($"{str.Value} {str.Key}");
+                    w.WriteLine($"{str.Value} {str.Key}");
             }
         }
     }
